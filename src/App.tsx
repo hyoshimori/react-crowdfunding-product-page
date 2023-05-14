1b5f1d4afd6015f71a44b0b7f61eb998c3daa165
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-05-14 09:59:16 +0900

    router removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Project from './components/main/Project';
-import Detail from './components/main/Detail';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Project />} />
-        <Route path="/detail" element={<Detail />} />
-      </Routes>
-    </Router>
+    <>
+      <Project />
+    </>
   );
 }
 
