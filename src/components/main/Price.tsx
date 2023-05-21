6c518211a6370eb5a6b69d74c7bf775036ada9b7
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-05-21 15:35:06 +0900

    progress bar done

--- src/components/main/Price.tsx
+++ src/components/main/Price.tsx
@@ -5,18 +5,21 @@ import styles from '../style/Price.module.css';
 const Price = () => {
   return (
     <div className={styles.body}>
-      <div>
-        <p>$89,914</p>
-        <span>of $100,000 backerd</span>
+      <div className={styles.body__and__progress}>
+        <div>
+          <p>$89,914</p>
+          <span>of $100,000 backerd</span>
+        </div>
+        <div className={styles.middle__div}>
+          <p>5,007</p>
+          <span>total backers</span>
+        </div>
+        <div>
+          <p>56</p>
+          <span>days left</span>
+        </div>
       </div>
-      <div className={styles.middle__div}>
-        <p>5,007</p>
-        <span>total backers</span>
-      </div>
-      <div>
-        <p>56</p>
-        <span>days left</span>
-      </div>
+      <progress className={styles.progress} max="100" value="89">100%</progress>
     </div>
   );
 }
