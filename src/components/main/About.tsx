25ecf7e2b8b82ed4eeb69dd5e2036f9f72c76de6
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-05-26 19:26:31 +0900

    about section updated

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -43,8 +43,8 @@ const About = () => {
     <div className={styles.body}>
       <h2 className={styles.about__this__project}>About this project</h2>
       <div className={styles.about__upper}>
-        <span className={styles.grey__color}>The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.</span>
-        <span className={styles.grey__color}>Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand.</span>
+        <span className={`${styles.grey__color} ${styles.desciption__message}`}>The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.</span>
+        <span className={`${styles.grey__color} ${styles.desciption__message}`}>Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand.</span>
       </div>
 
       {perks.map((perk, index) => (
