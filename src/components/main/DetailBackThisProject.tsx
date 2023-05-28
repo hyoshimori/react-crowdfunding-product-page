3b1314051b4cedea1324db73248b7c514a0100b7
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-05-28 20:33:13 +0900

    Enter Your Pledge wip

--- src/components/main/DetailBackThisProject.tsx
+++ src/components/main/DetailBackThisProject.tsx
@@ -26,23 +26,37 @@ const DetailBackThisProject = () => {
         perks && perks.map((el, index) => {
           console.log(el.name)
           return(
-            <div className={styles.details__projects__upper__and__lower} onClick={() => handleClick(index)}>
-              <div key={index} className={styles.details__projects}>
-                {
-                  // when the index of the loop is same as the state, button is checked
-                  buttonToggele === index ? <RadioButtonCheckedIcon /> : <RadioButtonUncheckedIcon />
-                }
-                <div>
-                  <div className={styles.details__projects__upper}>
-                    <div className={styles.details__projects__upper__name__and__price}>
-                      <h2>{el.name}</h2>
-                      <span>Pledge {el.minPledge} or more</span>
+            <div className={styles.details__projects__and__enter__your__pledge}>
+              <div className={styles.details__projects__upper__and__lower} onClick={() => handleClick(index)}>
+                <div key={index} className={styles.details__projects}>
+                  {
+                    // when the index of the loop is same as the state, button is checked
+                    buttonToggele === index ? <RadioButtonCheckedIcon /> : <RadioButtonUncheckedIcon />
+                  }
+                  <div>
+                    <div className={styles.details__projects__upper}>
+                      <div className={styles.details__projects__upper__name__and__price}>
+                        <h2>{el.name}</h2>
+                        <span>Pledge {el.minPledge} or more</span>
+                      </div>
+                      <span>{el.numbersLeft} <span style={{  color: 'grey'}}>left</span></span>
                     </div>
-                    <span>{el.numbersLeft} <span style={{  color: 'grey'}}>left</span></span>
                   </div>
                 </div>
+                <span onClick={() => handleClick(index)} className={styles.details__projects__lower}>{el.description}</span>
               </div>
-              <span onClick={() => handleClick(index)} className={styles.details__projects__lower}>{el.description}</span>
+              {
+                buttonToggele === index ?
+                <div className={styles.body__EnterYourPledge}>
+                  <span style={{color: 'grey'}}>Enter your pledge</span>
+                  <div className={styles.body__EnterYourPledge__right}>
+                    <input type="text" placeholder={'$'  + el.minPledge.toString()}/>
+                    <button>Continue</button>
+                  </div>
+              </div>
+              :
+                <></>
+              }
             </div>
         )})
       }
